4ee6a6a816ed52d567aa36dd7fc557265358898e
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2019-12-04 13:00:44 +0100

    feat: added Apache 2 License file

--- memory_chunk.h
+++ memory_chunk.h
@@ -1,3 +1,23 @@
+/* Copyright 2019-present Daniel Vergeylen daniel@vergeylen.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MEMORY_CHUNK_H
+#define MEMORY_CHUNK_H
+
+#include <mutex>
+#include <queue>
+#include <algorithm>  /* std::max */
 
 /* This Memory Chunk Store creates, stores and deletes
  * chunks of memory, according to T type.
@@ -22,13 +42,6 @@
  * or always recycle (never release). In between situations
  * may occur according to a custom recycling policy.
  */
-#ifndef MEMORY_CHUNK_H
-#define MEMORY_CHUNK_H
-
-#include <mutex>
-#include <queue>
-#include <algorithm>  /* std::max */
-
 template <typename T, typename R, typename RArg, typename... TArgs >
 class MemoryChunkStore {
   std::queue<T*> available_chunks;
