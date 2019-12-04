64d2b854fe0318601b237e0f9b435477cd5432c6
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2019-12-04 15:41:28 +0100

    fix: removed spurious include

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,8 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <iostream>
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../submodules/doctest/doctest/doctest.h"
 
